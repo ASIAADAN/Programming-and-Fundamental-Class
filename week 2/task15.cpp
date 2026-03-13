14d4d01a58958643029e53c718826c3078034414
Asia <asiaadankhalid@gmail.com>
2026-03-13 23:23:16 +0500

    new files added

---
+++ week 2/task15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+int main() {
+cout<<"              *     *     *"<<endl;
+cout<<"               *     *     *"<<endl;
+cout<<"                *     *     *"<<endl;
+cout<<"               *     *     *"<<endl;
+cout<<"              *     *     *"<<endl;
+cout<<"               *     *     *"<<endl;
+cout<<"                *     *     *"<<endl;
+cout<<"      _________________________________"<<endl;
+cout<<"      *                               *"<<endl;
+cout<<"       *                             *"<<endl;
+cout<<"        *                           *"<<endl;
+cout<<"          *                       *"<<endl;
+cout<<"             *__________________*"<<endl;
+cout<<" WELCOME TO F()():)Y APPLICATITION"<<endl;
+
+}            
---
+++ week 2/task16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+int main() {
+
+system("color 01   ");
+cout<<"+__^__________,_____,_____,_________^_,"<<endl;
+cout<<" |  |||||||||  '_______'   |          ' 0"<<endl;
+cout<<" '+,____________________________________|"<<endl;
+cout<<"   ',________,________,______________'"<<endl;
+cout<<"     /  xxxxx/'|      /'"<<endl;       
+cout<<"    /  xxxxx/'|     /'"<<endl;
+cout<<"   /  xxxxx/'_____'"<<endl;
+cout<<"  /   xxxx/"<<endl;
+cout<<" /   xxxx/"<<endl;
+cout<<"(_______("<<endl;
+cout<<" '_____'"<<endl;
+
+
+}
